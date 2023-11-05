5d90f32a2ec0cea78b79b9d1b9a2bf91890cecaa
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-04 20:22:26 -0400

    sum array of numbers

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  const flatArray = array.flat(Infinity)
+  let total = 0
+  flatArray.forEach((el)=>{
+   
+    total += el
+  })
+return total
 }
-
+sumItems([1, 3, 3, 5, 5])
 module.exports = sumItems;
